b8a4dec7cd613690305d5cc07c33dd60d1e730c5
Amit Yadav <amityadv882@gmail.com>
2026-07-30 20:59:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num==0) return "0";
+        bool isNegative = num<0;
+        num = abs(num);
+        string result;
+        while(num>0)
+        {
+            int remainder = num%7;
+            result.push_back('0'+remainder);
+            num = num/7;
+        }
+        if(isNegative)  result.push_back('-');
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
