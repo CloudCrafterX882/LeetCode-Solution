dff09071a8dc752a676e9810bf419d473e376d7f
Amit Yadav <amityadv882@gmail.com>
2026-08-29 19:27:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.1%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        long long count =0;
+        int count =0;
         for(int i=0;i<jewels.length();i++)
         {
             for(int j=0;j<stones.length();j++)
