076454c7f8aace94a2bace15b2df29eab859e18d
Amit Yadav <amityadv882@gmail.com>
2026-09-12 23:33:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (99.15%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSquares(int n) {
+        if (isSquare(n)) return 1;
+
+        int temp = n;
+        while (temp % 4 == 0) {
+            temp /= 4;
+        }
+        if (temp % 8 == 7) return 4;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (isSquare(n - i * i)) return 2;
+        }
+
+        return 3;
+    }
+
+    private boolean isSquare(int n) {
+        int sq = (int) Math.sqrt(n);
+        return sq * sq == n;
+    }
+}
